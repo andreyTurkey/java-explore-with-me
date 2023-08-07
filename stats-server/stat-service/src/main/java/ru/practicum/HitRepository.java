58b33e0fa20800ad91e408da6fc96c7718f83669
Andrey <andrei4-09@mail.ru>
2023-08-07 15:59:04 +0300

    Смена названия модуля на stats-server

--- stats-server/stat-service/src/main/java/ru/practicum/HitRepository.java
+++ stats-server/stat-service/src/main/java/ru/practicum/HitRepository.java
@@ -21,7 +21,7 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
     List<ViewStats> findAllByUriWithoutDistinct(@Param("uris") Collection<String> uris,
                                                 @Param("start") LocalDateTime startDecoded,
                                                 @Param("end") LocalDateTime endDecoded);
-    
+
     @Query("SELECT new ru.practicum.ViewStats (h.app, h.uri, count(h.ip)) from Hit h " +
             "WHERE  h.timestamp between :start AND :end group by h.uri, h.app")
     List<ViewStats> findAllWithoutUrisWithoutDistinct(@Param("start") LocalDateTime startDecoded,
